d94e8932aaf7be20e587ce6c949d5c3c15b7225a
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-17 17:18:52 +0300

    completed assgnment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet<=400){
+    return 'This one is on me!';
+  }
+  else if (feet >2500){
+    return 'No can do.';
+  }
+  else if (feet >= 2000){
+  return 'I will gladly take your thirty bucks.';
+  }
+}
+ 
+
+function ternaryCheckCity(town){
+  // Write your code here!
+  return town === "NYC" ? "Ok, sounds good." : "No go."; 
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip) {
+    case (tip= 'generous'):
+      return 'Thank you so much.';
+      
+      break;
+      case (tip= 'not as generous'):
+        return 'Thank you.';
+  
+    default:
+      break;
+      case (tip ='thanks for everything'):
+        return 'Bye.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+  
 }
