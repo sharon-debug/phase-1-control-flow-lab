8e671ee24912bd34714533e888d15e7dec99023c
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-23 16:50:30 +0300

    completed assgnment

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-function scuberGreetingForFeet(feet){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-  if (feet<=400){
-    return 'This one is on me!';
-  }
-  else if (feet >2500){
-    return 'No can do.';
-  }
-  else if (feet >= 2000){
-  return 'I will gladly take your thirty bucks.';
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if(distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
 }
- 
 
-function ternaryCheckCity(town){
-  // Write your code here!
-  return town === "NYC" ? "Ok, sounds good." : "No go."; 
-  
+function ternaryCheckCity(city){
+  return (city == 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
 function switchOnCharmFromTip(tip){
-  // Write your code here!
   switch (tip) {
-    case (tip= 'generous'):
-      return 'Thank you so much.';
-      
+    case 'generous':
+      return "Thank you so much.";
       break;
-      case (tip= 'not as generous'):
-        return 'Thank you.';
-  
+    case  "not as generous":
+      return "Thank you.";
+      break;
     default:
-      break;
-      case (tip ='thanks for everything'):
-        return 'Bye.';
-
+      return "Bye.";
   }
-  
 }
